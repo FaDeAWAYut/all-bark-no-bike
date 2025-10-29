12035b8df6e6c2f73942ef425ce742f19001c1b4
SlightlyToasted <116000719+SlightlyToasted@users.noreply.github.com>
2025-10-29 09:27:19 +0700

    Spawn cough drop at random position

--- scenes/collectable/collectables_manager.gd
+++ scenes/collectable/collectables_manager.gd
@@ -2,4 +2,14 @@ extends Node
 
 @export var cough_drop_pool: Pool
 
+func _ready():
+	spawn_cough_drops(0.0, 1152.0)
+
 # reference to other drops
+
+func spawn_cough_drops(camera_y_position: float, screen_size_x: float):
+    var cough_drop = cough_drop_pool.get_object()
+    var drop_y_level = camera_y_position - 1500
+    var drop_x_level = screen_size_x * randf_range(0.3, 0.7)
+    cough_drop.position = Vector2(drop_x_level, drop_y_level)
+
