9fc17425f1f60995280e3acc69c2f184a768a521
Sasirasys <sasirasys@gmail.com>
2025-11-08 16:13:52 +0700

    feat: level1 intro animation
    + make boss direction start with 0 (straight)

--- scenes/titlepage/title_page.gd
+++ scenes/titlepage/title_page.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 
 
 func _on_level_1_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
+	get_tree().change_scene_to_file("res://scenes/main/level_1_intro.tscn")
 
 
 func _on_quit_button_pressed() -> void:
