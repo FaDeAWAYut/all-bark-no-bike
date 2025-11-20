728745dc0239a60a45a74e8186e23935afe0be4a
Vinita Jungwiwattanaporn <vinitajwp@gmail.com>
2025-11-20 14:30:01 +0700

    feat: balance game
    - separate SpeedManager for phase 1 and 2
    - reduce max speed of phase 1
    - set start speed and max speed of phase 2
    - make collectables move slower by adding `collectable_speed_multiplier`
    - make player take more damage on collision (10 to 20)

--- scenes/main/animation_player.gd
+++ scenes/main/animation_player.gd
@@ -1,11 +1,11 @@
 extends AnimationPlayer
 
 @onready var parallaxBG:Parallax2D = $"../ParallaxBG".get_child(0)
-@onready var startSpeed = 400
+@onready var startSpeed = 400 # start speed of phase 2; if start_speed of phase 2 in SpeedManager changes, we must change this too
 var currentSpeed = 0
 var scrollChangeStartTime = 6.5 # when to start scroll
 var scrollChangeEndTime = 10 # when to reach start speed of level 1
-
+ 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	parallaxBG.scroll_offset.y = 35 # makes the intro bg align with level 1
